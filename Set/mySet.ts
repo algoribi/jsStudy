3f64210d41c36d92ad86c8cc47a08ff34d8f43a0
[Reed]ShinhyeChoi <algoribi36@gmail.com>
2022-12-20 19:10:08 +0900

    feat

--- Set/mySet.ts
+++ Set/mySet.ts
@@ -1,63 +1,64 @@
-class MySet<T> {
-  private items: T[];
+class MySet {
+  private items : { [key : string] : string};
 
-  constructor(items?: T[]) {
-    if (items === undefined) {
-      this.items = [];
-    } else {
-    this.items = items;
-    }
+  constructor() {
+    this.items = {};
   }
 
-  add(element : T) {
+  add(element : string) {
     if (!this.has(element)) {
-      this.items.push(element);
+      this.items[element] = element;
       return true;
     }
     return false;
   }
 
-  delete(element: T) {
-    const idx = this.items.indexOf(element);
-    if (idx > -1) {
-      this.items.splice(idx, 1);
+  delete(element : string) {
+    if (this.has(element)) {
+      delete this.items[element];
       return true;
     }
     return false;
   }
 
-  has(element: T) {
-    if (this.items.indexOf(element) > -1) {
-      return true;
-    }
-    return false;
-  }
-
-  clear() {
-    this.items = [];
-    return undefined;
-  }
-
-  size() {
-    return this.items.length;
-  }
-  
-  isEmpty() {
-    if (this.items.length === 0) {
-      return true;
-    }
-    return false;
+  has(element : string) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
   }
 
   values() {
-    return Array.from(this.items);
+    return Object.values(this.items);
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  iterate() {
+    
   }
 }
 
-const test = new MySet<string>();
-test.add("hello");
-test.add("test");
-test.add("world");
+// const test = new MySet<string>();
+// test.add("hello");
+// test.add("test");
+// test.add("world");
 
-const tt = test.values();
-console.log(test);
+const t = new Set<string>();
+const tt = new MySet();
+tt.add("hello");
+tt.add("hello");
+tt.add("world");
+
+console.log("test : ", t);
+// console.log(t["string"]);
+
+// const tt = test.values();
+// console.log(test);
